e063bfc32386a605bac86cf9f2b73078fb96c7b7
RafaelTome22 <rafaeltome554@gmail.com>
2024-05-13 17:50:52 -0300

    Atualizacao do routes

--- projeto-react/src/PAGES/home/home.js
+++ projeto-react/src/PAGES/home/home.js
@@ -4,7 +4,7 @@ function HomePage() {
   const navigate = useNavigate();
   return (
     <div>
-      <h1 onClick={() => {navigate('/login')}}>Home Page</h1>
+      <h1>Home Page</h1>
     </div>
   );
 }
--- projeto-react/src/routes.js
+++ projeto-react/src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter} from 'react-router-dom';
-import LoginPage from './PAGES/Tlogin/log.js';
 import HomePage from './PAGES/home/home.js';
 import SignupPage from './PAGES/Tcadastro/cad.js';
 
@@ -8,7 +7,6 @@ function RoutesApp(){
     return(
         <BrowserRouter>  
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
                 <Route path="/" element={<SignupPage />} />
                 <Route path="/home" element={<HomePage />} />
             </Routes>
