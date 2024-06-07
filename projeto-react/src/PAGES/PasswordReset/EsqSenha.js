7820716848351b1ba3af1a89d74ee8bfa8a7567f
unknown <rafaeltome554@gmail.com>
2024-06-06 22:06:53 -0300

    update de diretorios

---
+++ projeto-react/src/PAGES/PasswordReset/EsqSenha.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/PasswordReset.css'
+
+function EsqSenha() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Olá, Mundo!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default EsqSenha;
--- projeto-react/src/routes.js
+++ projeto-react/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './PAGES/home/home.js';
 import SignupPage from './PAGES/Tcadastro/cad.js';
+import EsqSenha from './PAGES/PasswordReset/EsqSenha.js'
 import PrivateRoute from './PrivateRoute';
 
 function RoutesApp() {
@@ -9,6 +10,7 @@ function RoutesApp() {
     <Routes>
       <Route path="/" element={<SignupPage />} />
       <Route path="/home" element={<PrivateRoute element={HomePage} />} />
+      <Route path="/PasswordReset" element={<EsqSenha />} />
     </Routes>
   );
 }
