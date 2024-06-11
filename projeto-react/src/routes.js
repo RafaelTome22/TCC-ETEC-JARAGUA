c6a893f91cd091cbb615569724119931810724be
RafaelTome22 <rafaeltome554@gmail.com>
2024-06-11 18:14:54 -0300

    criando quiz

--- projeto-react/src/routes.js
+++ projeto-react/src/routes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './PAGES/home/home.js';
 import SignupPage from './PAGES/Tcadastro/cad.js';
 import EsqSenha from './PAGES/PasswordReset/EsqSenha.js'
+import Quiz from './PAGES/quizPage/quiz.js'
 import PrivateRoute from './PrivateRoute';
 
 function RoutesApp() {
@@ -11,6 +12,7 @@ function RoutesApp() {
       <Route path="/" element={<SignupPage />} />
       <Route path="/home" element={<PrivateRoute element={HomePage} />} />
       <Route path="/PasswordReset" element={<EsqSenha />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
   );
 }
