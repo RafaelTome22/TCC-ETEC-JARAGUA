9d6e7a9f91a3d8843e1880afaad5c9c8f4ce08c4
unknown <rafaeltome554@gmail.com>
2024-06-17 20:33:28 -0300

    Styles das paginas principais

--- projeto-react/src/routes.js
+++ projeto-react/src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './PAGES/home/home.js';
+import Visao from './PAGES/ViewApi/Visao.js'
+import Auditiva from './PAGES/ViewApi/Auditiva.js'
+import Motora from './PAGES/ViewApi/Motora.js'
 import SignupPage from './PAGES/Tcadastro/cad.js';
 import EsqSenha from './PAGES/PasswordReset/EsqSenha.js'
 import Quiz from './PAGES/quizPage/quiz.js'
@@ -12,6 +15,9 @@ function RoutesApp() {
     <Routes>
       <Route path="/" element={<SignupPage />} />
       <Route path="/home" element={<PrivateRoute element={HomePage} />} />
+      <Route path="/Visao" element={<Visao /> }  />
+      <Route path="/Motora" element={<PrivateRoute element={Motora} />} />
+      <Route path="/Auditiva" element={<PrivateRoute element={Auditiva} />} />
       <Route path="/PasswordReset" element={<EsqSenha />} />
       <Route path="/quiz" element={<PrivateRoute element={Quiz} />} />
       <Route path="/rgb" element={<PrivateRoute element={ColorPicker} />} />
