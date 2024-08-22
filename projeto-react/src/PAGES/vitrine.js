654d178725fbee764da03670a7c629defc1eb283
unknown <rafaeltome554@gmail.com>
2024-08-22 17:10:56 -0300

    tela vitrine 90% em estilo

--- projeto-react/src/PAGES/vitrine.js
+++ projeto-react/src/PAGES/vitrine.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from '../styles/vitrine.module.css';
 import logo from '../assets/logo.png';
 import lupa from '../assets/lupa.png';
+import copy from '../assets/documento.png'
 
 function Vitrine() {
+  const codeRef = useRef(null); // Cria uma referência para o código
+
+  function copyCode() {
+    const codeElement = codeRef.current; // Acessa o elemento <pre> através da ref
+
+    // Cria um elemento de input temporário para usar como intermediário
+    const tempInput = document.createElement('textarea');
+    tempInput.value = codeElement.textContent;
+    document.body.appendChild(tempInput);
+
+    // Seleciona o conteúdo do input e copia para a área de transferência
+    tempInput.select();
+    document.execCommand('copy');
+
+    // Remove o input temporário
+    document.body.removeChild(tempInput);
+
+    // Feedback visual ou de áudio para indicar que o código foi copiado
+    alert('Código copiado para a área de transferência!');
+  }
+
   return (
-    <div className={styles.all}>  {/*Começo da página vitrine*/}
+    <div className={styles.all}> {/*Começo da página vitrine*/}
       <header className={styles['header-main']}> {/*Começo do header*/}
         <div className={styles['header-info']}> {/*Começo do header de info*/}
-          <img className={styles.logo}
-              src={logo}
-              alt='logo da empresa focos'
-          />
+          <img className={styles.logo} src={logo} alt="logo da empresa focos" />
           <p className={styles['nome-empresa']}>Focos</p>
         </div> {/*Fim do header de info*/}
         <div className={styles['header-pesquisa']}>
-          <input type='text' id='searchInput' placeholder='Pesquisar' className={styles['searchInput']} />
+          <input type="text" id="searchInput" placeholder="Pesquisar" className={styles['searchInput']} />
           <label htmlFor="searchInput">
             <span>
-              <img
-                className={styles['icon-pesquisa']}
-                src={lupa}
-                alt="ícone de pesquisa"
-              />
+              <img className={styles['icon-pesquisa']} src={lupa} alt="ícone de pesquisa" />
             </span>
           </label>
         </div>
         <div className={styles['header-btn']}> {/*Começo do header botoes de login e cadastro*/}
-          <button title='Cadastre-se' className={styles['btn-form-log']}>Cadastre-se</button>
-          <button title='Login' className={styles['btn-form-log']}>Login</button>
+          <button title="Cadastre-se" className={styles['btn-form-log']}>Cadastre-se</button>
+          <button title="Login" className={styles['btn-form-log']}>Login</button>
         </div> {/*Fim do header botoes de login e cadastro*/}
       </header> {/*Fim do header*/}
 
@@ -36,12 +51,27 @@ function Vitrine() {
         <div className={styles['article-main']}>
           <div className={styles['exemp-cod']}>
             <div className={styles['text-exemp-cod']}>
-                <h1>Exemplo de Código</h1>
-            </div> 
+              <h1>Primeiros passos</h1>
+              <div className={styles['code-container']}>
+                <pre ref={codeRef} id="code">
+                  {` npm install focos`}
+                </pre>
+                <button className={styles["btn-copy"]} onClick={copyCode}>
+                  <img
+                    className={styles['icon-copy']} src={copy} alt="ícone de cópia"
+                  />
+                </button>
+              </div>
+            </div>
           </div>
           <div className={styles['text-exemp']}>
-            
+            <div className={styles['explicacao-exemp-cod']}>
+              <h1>Sobre</h1>
+            </div>
           </div>
+        </div>
+        <div className="article-secundario">
+          
         </div>
       </article>
     </div>
