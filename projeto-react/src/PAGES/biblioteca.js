34ddc01a3f2722236a7bb22009d7f7122d71ab2a
unknown <rafaeltome554@gmail.com>
2024-08-25 19:51:31 -0300

    arrumando telas

--- projeto-react/src/PAGES/biblioteca.js
+++ projeto-react/src/PAGES/biblioteca.js
@@ -1,71 +1,55 @@
-import React from 'react';
-import '../styles/biblioteca.css'
-import logo from '../assets/logo.png'
-import lupa from '../assets/lupa.png'
-import user from '../assets/user.png'
-import mao from '../assets/mao.png'
-import olho from '../assets/olho.png'
-import livro from '../assets/livro.png'
-import orelha from '../assets/orelha.png'
-
+import React, { useRef } from 'react';
+import styles from '../styles/biblioteca.module.css';
+import logo from '../assets/logo.png';
+import lupa from '../assets/lupa.png';
+import copy from '../assets/documento.png'
 
 function Biblioteca() {
-    return (
-      <div className="all">  {/*Começo da página vitrine*/}
-        <header className="header-main"> {/*Começo do header*/}
-          <div className='header-info'> {/*Começo do header de info*/}
-          <img className='logo'
-                  src={logo}
-                  alt='logo da empresa focos'
-                  />
-              <p className='nome-empresa'>Focos</p>
-          </div> {/*Fim do header de info*/}
-          <div className='header-pesquisa'>
-          <input type='text' id='searchInput' placeholder='Pesquisar' />
-            <label for="searchInput">
-              <span >
-                <img
-                className='icon-pesquisa'
-                  src={lupa}
-                />
-              </span>
-            </label>
-          </div>
-          <img className = 'user'
-          src={user}
-          alt='imagem usuario'
-          />
+  const codeRef = useRef(null); // Cria uma referência para o código
 
-        <div className='retangulo-central'></div>
-        <div className='biblioteca-central'>Biblioteca</div>
+  function copyCode() {
+    const codeElement = codeRef.current; // Acessa o elemento <pre> através da ref
 
-          <div className= "retangulo-direito">
-          <img className = 'livro' src={livro} alt='imagem livro'/>
-          <br></br>
-          <img className = 'olho' src={olho} alt='imagem mao'/>
-          <br></br>
-          <img className = 'mao' src={mao} alt='imagem olho'/>
-          <br></br>
-          <img className = 'orelha' src={orelha} alt='imagem orelha'/>
-          </div>
+    // Cria um elemento de input temporário para usar como intermediário
+    const tempInput = document.createElement('textarea');
+    tempInput.value = codeElement.textContent;
+    document.body.appendChild(tempInput);
 
+    // Seleciona o conteúdo do input e copia para a área de transferência
+    tempInput.select();
+    document.execCommand('copy');
 
-          
+    // Remove o input temporário
+    document.body.removeChild(tempInput);
 
-        </header> {/*Fim do header*/}
-  
-        <article className='article-body'>
-          <div className='article-main'>
-            <div className=''></div>
-          </div>
-        </article>
-        
-  
-      </div>
-      //fim da página
-    );
+    // Feedback visual ou de áudio para indicar que o código foi copiado
+    alert('Código copiado para a área de transferência!');
   }
 
+  return (
+    <div className={styles.all}> {/*Começo da página vitrine*/}
+      <header className={styles['header-main']}> {/*Começo do header*/}
+        <div className={styles['header-info']}> {/*Começo do header de info*/}
+          <img className={styles.logo} src={logo} alt="logo da empresa focos" />
+          <p className={styles['nome-empresa']}>Focos</p>
+        </div> {/*Fim do header de info*/}
+        <div className={styles['header-pesquisa']}>
+          <input type="text" id="searchInput" placeholder="Pesquisar" className={styles['searchInput']} />
+          <label htmlFor="searchInput">
+            <span>
+              <img className={styles['icon-pesquisa']} src={lupa} alt="ícone de pesquisa" />
+            </span>
+          </label>
+        </div>
+        <div className={styles['header-btn']}> {/*Começo do header botoes de login e cadastro*/}
+          <button title="Cadastre-se" className={styles['btn-form-log']}>Cadastre-se</button>
+          <button title="Login" className={styles['btn-form-log']}>Login</button>
+        </div> {/*Fim do header botoes de login e cadastro*/}
+      </header> {/*Fim do header*/}
 
+      
+    </div>
+  );
+}
 
 export default Biblioteca;
