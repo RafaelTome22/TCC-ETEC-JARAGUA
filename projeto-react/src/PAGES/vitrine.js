939489b95fbea75902efa1f57ca505b1e0570c0e
RafaelTome22 <rafaeltome554@gmail.com>
2024-08-28 16:28:16 -0300

    atualizando botao e link

--- projeto-react/src/PAGES/vitrine.js
+++ projeto-react/src/PAGES/vitrine.js
@@ -1,10 +1,20 @@
 import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/vitrine.module.css';
 import logo from '../assets/logo.png';
 import lupa from '../assets/lupa.png';
-import copy from '../assets/documento.png'
+import copy from '../assets/documento.png';
 
 function Vitrine() {
+  const navigate = useNavigate();
+  const delay = 500; // Delay de 500 milissegundos (0.5 segundos)
+
+  const handleLogin = () => {
+    setTimeout(() => {
+      navigate('/login');
+    }, delay);
+  };
+
   const codeRef = useRef(null); // Cria uma referência para o código
 
   function copyCode() {
@@ -27,12 +37,12 @@ function Vitrine() {
   }
 
   return (
-    <div className={styles.all}> {/*Começo da página vitrine*/}
-      <header className={styles['header-main']}> {/*Começo do header*/}
-        <div className={styles['header-info']}> {/*Começo do header de info*/}
+    <div className={styles.all}> {/* Começo da página vitrine */}
+      <header className={styles['header-main']}> {/* Começo do header */}
+        <div className={styles['header-info']}> {/* Começo do header de info */}
           <img className={styles.logo} src={logo} alt="logo da empresa focos" />
           <p className={styles['nome-empresa']}>Focos</p>
-        </div> {/*Fim do header de info*/}
+        </div> {/* Fim do header de info */}
         <div className={styles['header-pesquisa']}>
           <input type="text" id="searchInput" placeholder="Pesquisar" className={styles['searchInput']} />
           <label htmlFor="searchInput">
@@ -41,11 +51,11 @@ function Vitrine() {
             </span>
           </label>
         </div>
-        <div className={styles['header-btn']}> {/*Começo do header botoes de login e cadastro*/}
+        <div className={styles['header-btn']}> {/* Começo do header botões de login e cadastro */}
           <button title="Cadastre-se" className={styles['btn-form-log']}>Cadastre-se</button>
-          <button title="Login" className={styles['btn-form-log']}>Login</button>
-        </div> {/*Fim do header botoes de login e cadastro*/}
-      </header> {/*Fim do header*/}
+          <button title="Login" className={styles['btn-form-log']} onClick={handleLogin}>Login</button>
+        </div> {/* Fim do header botões de login e cadastro */}
+      </header> {/* Fim do header */}
 
       <article className={styles['article-body']}>
         <div className={styles['article-main']}>
@@ -54,7 +64,7 @@ function Vitrine() {
               <h1>Primeiros passos</h1>
               <div className={styles['code-container']}>
                 <pre ref={codeRef} id="code">
-                  {` npm install focos`}
+                  {`npm install focos`}
                 </pre>
                 <button className={styles["btn-copy"]} onClick={copyCode}>
                   <img
@@ -71,7 +81,7 @@ function Vitrine() {
           </div>
         </div>
         <div className="article-secundario">
-          
+          {/* Conteúdo adicional pode ser adicionado aqui */}
         </div>
       </article>
     </div>
@@ -79,3 +89,4 @@ function Vitrine() {
 }
 
 export default Vitrine;
+
