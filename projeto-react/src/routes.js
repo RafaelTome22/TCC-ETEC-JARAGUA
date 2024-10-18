0ef06af7b72ab77e58206e06ff0f79354a5d66b6
unknown <rafaeltome554@gmail.com>
2024-10-17 21:28:31 -0300

    tudo funcionando

--- projeto-react/src/routes.js
+++ projeto-react/src/routes.js
@@ -1,31 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './PAGES/home/home.js';
 import SignupPage from './PAGES/Tcadastro/cad.js';
-import EsqSenha from './PAGES/PasswordReset/EsqSenha.js'
-
-
-
-
-import SignupPage from './PAGES/Tcadastro/cad.js';
-import EsqSenha from './PAGES/PasswordReset/EsqSenha.js'
 import Vitrine from './PAGES/vitrine.js';
 import Biblioteca from './PAGES/biblioteca.js';
 import Componentes from './PAGES/componentes.js';
 import RecuperaSenha from './PAGES/RecuperaSenha.js';
 
-
 function RoutesApp() {
   return (
     <Routes>
-      <Route path="/" element={<SignupPage />} />
+      <Route path="/" element={<Vitrine />} />
       <Route path="/biblioteca" element={<Biblioteca />} />
       <Route path="/componentes" element={<Componentes />} />
       <Route path="/login" element={<SignupPage />} />
-      <Route path="/PasswordReset" element={<EsqSenha/>} />
     </Routes>
   );
 }
 
 export default RoutesApp;
-
