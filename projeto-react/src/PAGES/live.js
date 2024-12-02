e0a4f6d857cf73dc3efeff7e96006568037560e0
unknown <PablitoDoPix>
2024-12-01 22:24:24 -0300

    Conteiners ajeitados, teste o componente renderizando botão... com outro botão.

--- projeto-react/src/PAGES/live.js
+++ projeto-react/src/PAGES/live.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'; 
 import { AccessibleButton } from 'focos'; 
 import styles from '../styles/TesteComponentes.module.css';
@@ -12,24 +12,39 @@ function AcesssibleButton() {
     <AccessibleButton label="Clique em mim" onClick={handleClick} />
   );
 }
-  
 `;
 
-    return (
-      <div className={styles['teste-componente-root']}>
-        <LiveProvider code={initialCode} scope={{ AccessibleButton }}>
-          <LiveEditor 
-            className={styles['code-block']} 
-            value={initialCode} 
-            readOnly 
-          />
-          <LiveError className={styles['error-text']} />
+    const [shouldRender, setShouldRender] = useState(false);
 
-          <div className={styles['preview-container']}>
-            <LivePreview />
-          </div>
-        </LiveProvider>
-      </div>
+    const handleRenderClick = () => {
+        setShouldRender(true);
+    };
+
+    return (
+        <div className={styles['teste-componente-root']}>
+            <div className={styles['editor-container']}>
+                <h1 className={styles['header']}>Console</h1>
+                <LiveProvider code={initialCode} scope={{ AccessibleButton }}>
+                    <LiveEditor className={styles['code-block']} value={initialCode} readOnly />
+                    <LiveError className={styles['error-text']} />
+                </LiveProvider>
+                <button 
+                    className={`${styles['btn']} ${styles['btn-render']}`} 
+                    onClick={handleRenderClick}
+                >
+                    Renderizar Componente
+                </button>
+            </div>
+
+            <div className={styles['preview-container']}>
+                <h1 className={styles['header']}>Componente Renderizado</h1>
+                {shouldRender && (
+                    <LiveProvider code={initialCode} scope={{ AccessibleButton }}>
+                        <LivePreview className={styles['live-preview']} />
+                    </LiveProvider>
+                )}
+            </div>
+        </div>
     );
 }
 
